c545b509d212450c3efeabbcf18e7d05b43495d1
ivoncone <posdatamexico@gmail.com>
2022-02-24 12:44:50 -0800

    create models

---
+++ books/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+	first_name = models.CharField(max_length=120, verbose_name='Nombre')
+	last_name = models.CharField(max_length=120, verbose_name='Apellido')
+	birth_date = models.DateField(verbose_name='Fecha de nacimiento')
+	created_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha creacion')
+
+	class Meta:
+		db_table = 'authors'
+
+
+class Book(models.Model):
+	name = models.CharField(max_length=128, verbose_name='Nombre')
+	isbn = models.IntegerField(default=0, verbose_name='ISBN')
+	publisher_date = models.DateField(verbose_name='fecha de publicacion')
+	created_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha creacion')
+	author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, verbose_name='Author')
+
+	class Meta:
+		db_table = 'books'
+
+		
--- periodicTable/settings.py
+++ periodicTable/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'books'
 ]
 
 MIDDLEWARE = [
@@ -79,6 +81,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+"""DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'ivonne',
+        'PASSWORD': 'mexicali',
+        'HOST': 'localhost',
+        'PORT': '5482'
+    }
+}"""
 
 
 # Password validation
